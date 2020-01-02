96e481c8bb0e49de46372bfd4de97d1699c9d39e
Bibhash7 <59429659+Bibhash7@users.noreply.github.com>
2020-01-02 10:51:05 +0530

    Add files via upload

---
+++ Non Fibonacci Numbers.cpp
@@ -0,0 +1,30 @@
+//Program to Generate Non Fibonacci Numbers upto nth Fibonacci
+#include<bits/stdc++.h>
+using namespace std;
+int fiboEven(int n)
+{
+	int a=1,b=1,c,j=1;
+	for(int i=1;i<=n;i++)
+	{
+		
+		c=a+b;
+		a=b;
+		b=c;
+		while(j<a && i!=n)
+		{
+			cout<<j<<" ";
+			j++;
+		}
+		j++;
+		
+	}
+}
+int main()
+{
+	int n;
+	cout<<"Enter Range:";
+	cin>>n;
+	cout<<"Non fibonacci Numbers are:"<<endl;
+	fiboEven(n);
+	return 0;
+}
